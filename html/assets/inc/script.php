0ee4eed3861c138da4865b512749a05933b5096c
Naoko Nishimura on fsimg <nishimura@fsimg.jp>
2016-03-16 19:27:33 +0900

    tst

--- html/assets/inc/script.php
+++
@@ -1,3 +0,0 @@
-<script src="<?php echo $local_path; ?>assets/js/library/lib.js"></script>
-<script src="<?php echo $local_path; ?>assets/js/common.js"></script>
-	
--- html/assets/js/utility-ck.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $("a[href^=#]").click(function() {
-        var e = 700, t = $(this).attr("href"), n = $(t == "#" || t == "" ? "html" : t), r = n.offset().top;
-        $($.browser.safari ? "body" : "html").animate({
-            scrollTop: r
-        }, e, "easeInOutQuart");
-        return !1;
-    });
-});
