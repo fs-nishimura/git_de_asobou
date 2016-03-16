ecc0720e2980596ae78bcef11a5ef6b33c4d56fe
Naoko Nishimura on fsimg <nishimura@fsimg.jp>
2016-03-16 19:28:49 +0900

    tst

--- html/assets/js/body100.js
+++
@@ -1,9 +0,0 @@
-$(window).load(function() {
-    var winH = $(window).height();
-    var bodyH = $("body").innerHeight() - 84;
-    var contentsH = $("#main").height();
-    if (winH > bodyH) {
-        var newContentsH = winH - bodyH + contentsH;
-        $("#main").height(newContentsH);
-    }
-});
--- html/assets/js/news.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    var prevBtn = $("a[rel='prev']");
-    var nextBtn = $("a[rel='next']");
-    if (!nextBtn.length) {
-        $(".wp-pagenavi").append('<a class="nextpostslink disabled" rel="next" href="javascript:void(0);"><img src="/img/news/index/btn-next.png" alt="NEXT" width="39" height="10"></a>');
-    } else if (!prevBtn.length) {
-        $(".wp-pagenavi").prepend('<a class="previouspostslink disabled" rel="prev" href="javascript:void(0);"><img src="/img/news/index/btn-prev.png" alt="PREV" width="38" height="10"></a>');
-    }
-});
